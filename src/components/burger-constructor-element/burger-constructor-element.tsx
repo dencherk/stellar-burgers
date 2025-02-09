32401f3fab9f6414f0de069ab6573906737f3410
Denis Cherkasskiy <cherkasskiydenis@yandex.ru>
2025-02-09 10:55:07 +0300

    relise

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,26 @@
 import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import { useDispatch } from '../../services/store';
+import {
+  removeFromConstructor,
+  reorderConstructor
+} from '../../services/slices/builder';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
+    const handleMoveDown = () => {
+      dispatch(reorderConstructor({ from: index, to: index + 1 }));
+    };
 
-    const handleMoveUp = () => {};
+    const handleMoveUp = () => {
+      dispatch(reorderConstructor({ from: index, to: index - 1 }));
+    };
 
-    const handleClose = () => {};
+    const handleClose = () => {
+      dispatch(removeFromConstructor(Number(ingredient.id)));
+    };
 
     return (
       <BurgerConstructorElementUI
