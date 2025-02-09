8e3cbd53734416cdfa8c37d0de91b9b7d849113e
Denis Cherkasskiy <cherkasskiydenis@yandex.ru>
2025-02-09 11:06:54 +0300

    relise

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -14,13 +14,6 @@ import { OrderInfo } from '../order-info';
 import { IngredientDetails } from '../ingredient-details';
 import '../../index.css';
 import styles from './app.module.css';
-import {
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-  useMatch
-} from 'react-router-dom';
 import { useEffect, useCallback } from 'react';
 import { getIngredients } from '../../services/slices/ingredients';
 import { clearSelectedOrder } from '../../services/slices/feeds';
@@ -28,6 +21,13 @@ import { AppHeader } from '@components';
 import { useDispatch } from '../../services/store';
 import { getUser } from '../../services/slices/user';
 import { ProtectedRoute } from '../protected-route';
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  useMatch
+} from 'react-router-dom';
 
 const App = () => {
   const dispatch = useDispatch();
